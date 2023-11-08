37e8057d54296d9786e93810c0e1d39d1ae27ff5
gkxornjs <cjfcjf00@naver.com>
2023-11-08 21:43:58 +0900

    #hataekwon

--- JavaProject_game/src/view/Intro.java
+++ JavaProject_game/src/view/Intro.java
@@ -1,18 +1,18 @@
 package view;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
-public class Intro extends JFrame{
-	public Intro() {
-		setTitle("");
-		
-		setSize(1600, 900);
-		setVisible(true);
-	}
+public class Intro extends JPanel{
+ImageIcon introIc = new ImageIcon(""); // 인트로 이미지
 	
-	public static void main(String[] args) {
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g); // 화면을 비운다
+		
+		g.drawImage(introIc.getImage(), 0, 0, null);
+	}
+	public Intro() {
 		
 	}
-
 }
 //시작 화면
