5004e604cf56583358c556fb5420a3bc6b6387b7
dealer0519 <banana169@naver.com>
2023-11-08 20:18:11 +0900

    Update Intro.java

--- JavaProject_game/src/view/Intro.java
+++ JavaProject_game/src/view/Intro.java
@@ -5,6 +5,9 @@ import javax.swing.JFrame;
 public class Intro extends JFrame{
 	public Intro() {
 		setTitle("");
+		
+		setSize(1600, 900);
+		setVisible(true);
 	}
 	
 	public static void main(String[] args) {
