4afcfb8e2e6da94823485c1d694dff2dcd136be3
gkxornjs <cjfcjf00@naver.com>
2023-11-14 00:25:44 +0900

    htk

--- JavaProject_game/src/view/Game.java
+++ JavaProject_game/src/view/Game.java
@@ -1,5 +1,15 @@
 package view;
 
-public class Game {
+import java.awt.Graphics;
 
+import javax.swing.*;
+
+public class Game extends JPanel {
+	private ImageIcon backIc = new ImageIcon("img/background/MainGame.png"); //게임 배경화면
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g); // 화면을 비운다
+			
+		g.drawImage(backIc.getImage(), 0, 0, null);
+		
+	}
 }
