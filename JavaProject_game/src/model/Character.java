21e73adc6012d530f27ce5f71db19f01592e0923
huni-mir <149519115+huni-mir@users.noreply.github.com>
2023-11-13 22:40:21 +0900

    #4 char

--- JavaProject_game/src/model/Character.java
+++ JavaProject_game/src/model/Character.java
@@ -1,6 +1,14 @@
 package model;
 
-public class Character {
+import java.awt.*;
+import javax.swing.*;
 
+public class Character {
+	private JLabel cat = new JLabel("img/character/catFont.png");
+	private JLabel rabbit = new JLabel("img/character/rabbitFont.png");
+	private JLabel bear = new JLabel("img/character/bearFont.png");
+	private JLabel astronaut = new JLabel("img/character/astronautFont.png"); 
+	
+		
 }
 	
