61d10e15c4f0fb54a8aedd827a4dc00847292352
gkxornjs <cjfcjf00@naver.com>
2023-11-16 19:31:59 +0900

    htk

--- JavaProject_game/src/view/GameMap.java
+++ JavaProject_game/src/view/GameMap.java
@@ -28,7 +28,7 @@ public class GameMap extends JPanel{
 		g.drawImage(MainGame.getImage(), 0, 0, null);
 		
 	}
-	private JLabel beullog1 = new JLabel(new ImageIcon("img/block/Block.png"));
+	private JLabel beullog1 = new JLabel(new ImageIcon("img/block/PupleBlock.png"));
 	private JLabel beullog2 = new JLabel(new ImageIcon("img/block/BlueBlock.png"));
 	private JLabel beullog3 = new JLabel(new ImageIcon("img/block/PinkBlock.png"));
 	private JLabel beullog4 = new JLabel(new ImageIcon("img/block/RedBlock.png"));
@@ -37,19 +37,56 @@ public class GameMap extends JPanel{
 
 	private GameMap() {
 		setLayout(null);
-		 Beullog beullog = new Beullog(12,16);
+		 Beullog beullog = new Beullog(16,12);
 	        for (int i = 0; i < beullog.size; i++) {
 	           for (int j = 0; j < beullog.size1; j++) {
 	        	   beullog.map[i][j] = "1";
 	              String block = beullog.map[i][j];
-	              if ((i == 0 && j <3) || (i==0 && (j>12 && j<16))) {
+	              if (i < 3 && j ==0 || ((i>12 && i<16)&& j==0)/*1번쨰열*/ 
+	            	|| i<1  && j ==1 || ((i>14 && i<16)&& j==1)/*2번쨰열*/
+	            	|| i<1  && j ==10 || ((i>14 && i<16)&& j==10)/*11번쨰열*/
+	            	|| i<3  && j ==11 || ((i>12 && i<16)&& j==11)/*12번쨰열*/) {
 	                 JLabel beullog1 = new JLabel(new ImageIcon("img/block/RedBlock.png"));
-	                 
+	                 //item.add 자리
 	                 this.add(beullog1);
-	                 beullog1.setBounds(i * 70 + 12, j * 40+16, 75, 75);
-	              } break;
-
+	                 beullog1.setBounds(i * 75, j * 75, 75, 75);
+	              } else if(i < 3 && j ==3 /*4번쨰열*/ 
+			            	|| (i<2 && i>0)  && (j >3 && j<7)/*5,6,7번쨰열*/
+			            	|| i<2  && j ==7 /*8번쨰열*/) {
+			                 JLabel beullog5 = new JLabel(new ImageIcon("img/block/SkyBlock.png"));
+			                 //item.add 자리
+			                 this.add(beullog5);
+			                 beullog5.setBounds(i * 75, j * 75, 75, 75); //J
+			              } else if((i>3&&i<6) && j ==3 /*4번쨰열*/ 
+					            	|| i==3 && j==4 ||  i==6 && j==4  /*5번쨰열*/
+					            	|| (i>2 &&i<7) && j==5  /*6번쨰열*/
+					            	|| i==3 && j==6 ||  i==6 && j==6  /*7번쨰열*/
+					            	|| i==3 && j==7 ||  i==6 && j==7  /*8번쨰열*/) {
+					                 JLabel beullog3 = new JLabel(new ImageIcon("img/block/PinkBlock.png"));
+					                 //item.add 자리
+					                 this.add(beullog3);
+					                 beullog3.setBounds(i * 75, j * 75, 75, 75);//A
+					              } else if(i==7 && j==3 ||  i==11 && j==3 /*4번쨰열*/ 
+							            	|| i==7 && j==4 ||  i==11 && j==4  /*5번쨰열*/
+							            	|| i==7 && j==5 ||  i==11 && j==5  /*6번쨰열*/
+							            	|| i==8 && j==6 ||  i==10 && j==6  /*7번쨰열*/
+							            	|| i==9 && j==7 /*8번쨰열*/) {
+							                 JLabel beullog2 = new JLabel(new ImageIcon("img/block/BlueBlock.png"));
+							                 //item.add 자리
+							                 this.add(beullog2);
+							                 beullog2.setBounds(i * 75, j * 75, 75, 75);//V
+							              } else if((i>12&&i<15) && j ==3 /*4번쨰열*/ 
+									            	|| i==12 && j==4 ||  i==15 && j==4  /*5번쨰열*/
+									            	|| (i>11 &&i<16) && j==5  /*6번쨰열*/
+									            	|| i==12 && j==6 ||  i==15 && j==6  /*7번쨰열*/
+									            	|| i==12 && j==7 ||  i==15 && j==7  /*8번쨰열*/) {
+									                 JLabel beullog1 = new JLabel(new ImageIcon("img/block/PupleBlock.png"));
+									                 //item.add 자리
+									                 this.add(beullog1);
+									                 beullog1.setBounds(i * 75, j * 75, 75, 75);//A
+									              }
 	           }
-		      }
-}
+	           }
+	        
+	           }
 }
--- JavaProject_game/src/view/TestMap.java
+++ JavaProject_game/src/view/TestMap.java
@@ -26,7 +26,7 @@ public class TestMap extends JFrame {
 			g.drawImage(MainGame.getImage(), 0, 0, null);
 			
 		}
-		private JLabel beullog1 = new JLabel(new ImageIcon("img/block/Block.png"));
+		private JLabel beullog1 = new JLabel(new ImageIcon("img/block/PupleBlock.png"));
 		private JLabel beullog2 = new JLabel(new ImageIcon("img/block/BlueBlock.png"));
 		private JLabel beullog3 = new JLabel(new ImageIcon("img/block/PinkBlock.png"));
 		private JLabel beullog4 = new JLabel(new ImageIcon("img/block/RedBlock.png"));
@@ -49,15 +49,40 @@ public class TestMap extends JFrame {
 		                 this.add(beullog1);
 		                 beullog1.setBounds(i * 75, j * 75, 75, 75);
 		              } else if(i < 3 && j ==3 /*4번쨰열*/ 
-				            	|| i<3  && j ==1 || ((i>14 && i<16)&& j==1)/*5번쨰열*/
-				            	|| i<1  && j ==10 || ((i>14 && i<16)&& j==10)/*6번쨰열*/
-				            	|| i<3  && j ==11 || ((i>12 && i<16)&& j==11)/*7번쨰열*/
-				            	|| i<3  && j ==11 || ((i>12 && i<16)&& j==11)/*8번쨰열*/) {
+				            	|| (i<2 && i>0)  && (j >3 && j<7)/*5,6,7번쨰열*/
+				            	|| i<2  && j ==7 /*8번쨰열*/) {
 				                 JLabel beullog5 = new JLabel(new ImageIcon("img/block/SkyBlock.png"));
 				                 //item.add 자리
 				                 this.add(beullog5);
-				                 beullog5.setBounds(i * 75, j * 75, 75, 75);
-				              } 
+				                 beullog5.setBounds(i * 75, j * 75, 75, 75); //J
+				              } else if((i>3&&i<6) && j ==3 /*4번쨰열*/ 
+						            	|| i==3 && j==4 ||  i==6 && j==4  /*5번쨰열*/
+						            	|| (i>2 &&i<7) && j==5  /*6번쨰열*/
+						            	|| i==3 && j==6 ||  i==6 && j==6  /*7번쨰열*/
+						            	|| i==3 && j==7 ||  i==6 && j==7  /*8번쨰열*/) {
+						                 JLabel beullog3 = new JLabel(new ImageIcon("img/block/PinkBlock.png"));
+						                 //item.add 자리
+						                 this.add(beullog3);
+						                 beullog3.setBounds(i * 75, j * 75, 75, 75);//A
+						              } else if(i==7 && j==3 ||  i==11 && j==3 /*4번쨰열*/ 
+								            	|| i==7 && j==4 ||  i==11 && j==4  /*5번쨰열*/
+								            	|| i==7 && j==5 ||  i==11 && j==5  /*6번쨰열*/
+								            	|| i==8 && j==6 ||  i==10 && j==6  /*7번쨰열*/
+								            	|| i==9 && j==7 /*8번쨰열*/) {
+								                 JLabel beullog2 = new JLabel(new ImageIcon("img/block/BlueBlock.png"));
+								                 //item.add 자리
+								                 this.add(beullog2);
+								                 beullog2.setBounds(i * 75, j * 75, 75, 75);//V
+								              } else if((i>12&&i<15) && j ==3 /*4번쨰열*/ 
+										            	|| i==12 && j==4 ||  i==15 && j==4  /*5번쨰열*/
+										            	|| (i>11 &&i<16) && j==5  /*6번쨰열*/
+										            	|| i==12 && j==6 ||  i==15 && j==6  /*7번쨰열*/
+										            	|| i==12 && j==7 ||  i==15 && j==7  /*8번쨰열*/) {
+										                 JLabel beullog1 = new JLabel(new ImageIcon("img/block/PupleBlock.png"));
+										                 //item.add 자리
+										                 this.add(beullog1);
+										                 beullog1.setBounds(i * 75, j * 75, 75, 75);//A
+										              }
 		           }
 		           }
 		        
