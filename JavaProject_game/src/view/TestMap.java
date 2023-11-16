aa5091a3ec8c3bd7c8303d20d38ae0baa0bec8e9
gkxornjs <cjfcjf00@naver.com>
2023-11-17 00:12:15 +0900

    htk

--- JavaProject_game/src/view/TestMap.java
+++ JavaProject_game/src/view/TestMap.java
@@ -43,7 +43,7 @@ public class TestMap extends JFrame {
 					beullog.map[i][j] = "1";
 					String block = beullog.map[i][j];
 					if (i < 3 && j == 0 || ((i > 12 && i < 16) && j == 0)/* 1번쨰열 */
-							|| i < 1 && j == 1 || ((i > 14 && i < 16) && j == 1)/* 2번쨰열 */
+							 || i < 1 && j == 1 || ((i > 14 && i < 16) && j == 1)/* 2번쨰열 */
 							|| i < 1 && j == 10 || ((i > 14 && i < 16) && j == 10)/* 11번쨰열 */
 							|| i < 3 && j == 11 || ((i > 12 && i < 16) && j == 11)/* 12번쨰열 */) {
 						JLabel beullog1 = new JLabel(new ImageIcon("img/block/RedBlock.png"));
@@ -52,7 +52,7 @@ public class TestMap extends JFrame {
 						beullog1.setBounds(i * 75, j * 75, 75, 75);
 					} else if (i < 3 && j == 3 /* 4번쨰열 */
 							|| (i < 2 && i > 0) && (j > 3 && j < 7)/* 5,6,7번쨰열 */
-							|| i < 2 && j == 7 /* 8번쨰열 */) {
+								|| i < 2 && j == 7 /* 8번쨰열 */) {
 						JLabel beullog5 = new JLabel(new ImageIcon("img/block/SkyBlock.png"));
 						// item.add 자리
 						this.add(beullog5);
