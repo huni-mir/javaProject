08b2404c9a153e4974cb8a17c2bea34ba63bc03c
huni-mir <149519115+huni-mir@users.noreply.github.com>
2023-11-16 19:11:51 +0900

    #1

---
+++ JavaProject_game/src/model/CharacterAbility.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.awt.Image;
+
+public class CharacterAbility { //캐릭터 능력치
+	private Image image; //캐릭터 이미지
+	
+	//캐릭터 체력
+	private int char1p = 300;	//1P
+	private int char2p = 300;	//2P
+	
+	//캐릭터 능력 
+	private int speed1p = 300;	//2P
+	private int speed2p = 300;	//2P
+	private int bomb1p = 300;	//2P
+	private int bomb2p = 300;	//2P
+	private int range1p = 300;	//2P
+	private int range2p = 300;	//2P
+	private int char2p = 300;	//2P
+	private int char2p = 300;	//2P
+	
+	
+}
